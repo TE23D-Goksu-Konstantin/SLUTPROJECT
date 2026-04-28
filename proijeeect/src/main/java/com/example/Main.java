19fe0728c1fc8d155713dd59cf2eadbcb4aeb410
Konstantin Göksu <konstantin.goksu@elev.ga.ntig.se>
2026-04-28 13:06:24 +0200

    Småändringar för E-del

--- proijeeect/src/main/java/com/example/Main.java
+++ proijeeect/src/main/java/com/example/Main.java
@@ -1,13 +1,10 @@
 package com.example;
 
-import com.google.gson.Gson;
+import com.google.gson.*;
 import com.google.gson.GsonBuilder;
 
-import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
 
@@ -16,8 +13,7 @@ public class Main
 {
     public static void main(String[] args) throws MalformedURLException 
     {
-
-
+    
     URL url = new URL("http://10.151.168.5:3114/");
     Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
